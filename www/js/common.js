ac1e9bc589a599fa8886a1de664abdf5c4437a58
unknown <>
2016-07-04 00:55:57 +0900

    js整理

--- www/js/common.js
+++ www/js/common.js
@@ -3,20 +3,27 @@ var intervalID = null;
 var randomNum = 0;
 // カウントアップやカウントダウンをするユーザー定義関数
 function countUpDown( num , randomNum) {
-  var sampleOutput = document.getElementById( "sampleOutput" );
-  sampleOutput.innerHTML = parseInt( sampleOutput.innerHTML ) + num;
-  // if ((parseInt( sampleOutput.innerHTML ) + num) < 10) {
-    // alert("バン");
-              // clearInterval( intervalID );
-  // } else {
-  resizeImagePercent(parseFloat( sampleOutput.innerHTML ) + num);
     
-  // }
+    console.log("num");
+    console.log(num);
+    console.log("randomNum");
+    console.log(randomNum);
+    
+    var countTime = $(".countTime").val();
+    var sampleOutput = document.getElementById( "sampleOutput" );
+    sampleOutput.innerHTML = parseInt( sampleOutput.innerHTML ) + num;
+    // if ((parseInt( sampleOutput.innerHTML ) + num) < 10) {
+        // alert("バン");
+                            // clearInterval( intervalID );
+    // } else {
+    resizeImagePercent(countTime);
+        
+    // }
 }
 
 // 画像を引数倍する
 function resizeImagePercent( resizeRate ) {
-  var resizeImg = document.getElementById("sampleA");
-  resizeImg.width = resizeImg.naturalWidth * resizeRate;
-  resizeImg.height = resizeImg.naturalHeight * resizeRate;
+    var resizeImg = document.getElementById("balloon");
+    resizeImg.width = resizeImg.naturalWidth * resizeRate;
+    resizeImg.height = resizeImg.naturalHeight * resizeRate;
 }
